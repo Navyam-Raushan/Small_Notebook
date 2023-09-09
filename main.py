2208649a779bc7935a02830b73c9756b4463b757
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-09 18:33:55 +0530

    final formatting done added footer.

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 # an object for pdf
 pdf = FPDF(orientation="L", format="a4", unit="mm")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
+
+    # SETTING HEADER
     # adding new page to this pdf
     pdf.add_page()
 
@@ -23,12 +26,25 @@ for index, row in df.iterrows():
     # we need a line and it will be decided by x and y axis so give coordinated of starting and ending points
     pdf.line(11, 27, 270, 27)
 
+    # SETTING FOOTER
+    # This will add 265 break lines from above header and write same topic name.
+    pdf.ln(160)
+    pdf.set_font(family="Times", style="I", size=9)
+    pdf.set_text_color(190, 200, 180)
+    pdf.cell(w=0, ln=1, h=9, border=0, txt=row["Topic"], align="R")
+
     # adding blank pages for each topic as we need nested loop
     """Adding this feature need to iterate over the no. of pages each time for each topic
        one page is added earlier so we subtract 1 here.
     """
     for j in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # SETTING THE FOOTER
+        pdf.ln(160 + 25)
+        pdf.set_font(family="Times", style="I", size=9)
+        pdf.set_text_color(190, 200, 180)
+        pdf.cell(w=0, ln=1, h=9, border=0, txt=row["Topic"], align="R")
 
 
 # to shown output name of file is parameter
